35405815fd2b47267218c90bd78e5b1198fb18b9
AmumuSeong <rmawkxkd44@naver.com>
2021-06-11 12:17:14 +0900

    1234

--- screen/webERP/src/main/java/com/myspring/systemmag2/service/SystemmagService.java
+++
@@ -1,7 +0,0 @@
-package com.myspring.systemmag2.service;
-
-import com.myspring.systemmag.vo.SystemmagVO;
-
-public interface SystemmagService {
-	public int addCompany(SystemmagVO systemmagVO) throws Exception;
-}
