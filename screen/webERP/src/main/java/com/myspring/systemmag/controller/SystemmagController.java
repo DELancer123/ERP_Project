4f030f897cd88d6d5668b5c8dad4cf6e56b35069
DESKTOP-JE83F0J\SHY-702-18 <xellos_13@naver.com>
2021-06-11 16:07:29 +0900

    인터페이스 추가구현

--- screen/webERP/src/main/java/com/myspring/systemmag/controller/SystemmagController.java
+++ screen/webERP/src/main/java/com/myspring/systemmag/controller/SystemmagController.java
@@ -9,8 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.myspring.productionBaseInfo.BOM.vo.bomVO;
 import com.myspring.systemmag.vo.SystemmagVO;
-
+//회사등록
 public interface SystemmagController {
 	public ModelAndView addCompany (@ModelAttribute("company") SystemmagVO company,HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView viewCompany(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView delCompany(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView adressZip(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	//사업장등록
+	public ModelAndView addWorkplace(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView viewWorkplace(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView updWorkplace(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView delWorkplace(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	//부서등록
+	public ModelAndView addDepartment(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	
 }
