a293c333b241ca68c7ad6c24b48c528a47a96373
DESKTOP-JE83F0J\SHY-702-18 <xellos_13@naver.com>
2021-06-11 12:35:36 +0900

    no message

--- screen/webERP/src/main/java/com/myspring/systemmag2/dao/SystemmagDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myspring.systemmag2.dao;
-
-import org.springframework.dao.DataAccessException;
-
-import com.myspring.systemmag.vo.SystemmagVO;
-
-public interface SystemmagDAO {
-	public int insertNewCompany (SystemmagVO systemmagVO) throws DataAccessException;
-}
