72881049223e12f3f2fd5b42058708c4f2f151fd
DeLancer <rudrms950@gmail.com>
2021-06-22 18:45:09 +0900

    작업지시확정 자재출고 구현중

--- screen/webERP/src/main/java/com/myspring/commonProduction/operationRegist/dao/OperationRegistDAOImpl.java
+++ screen/webERP/src/main/java/com/myspring/commonProduction/operationRegist/dao/OperationRegistDAOImpl.java
@@ -184,7 +184,12 @@ public class OperationRegistDAOImpl implements OperationRegistDAO{
 	public List selectReleaseDetail(String number) throws DataAccessException, ParseException {
 		List<OperationRegistVO> infoList = null;
 		
-		infoList = sqlSession.selectList("mappers.erp.selectReleaseDetail", number);		
+		String count = sqlSession.selectOne("mappers.erp.countWorkOrderNumber",number);
+		if(count.equals("0")) {
+			infoList = sqlSession.selectList("mappers.erp.selectReleaseDetail", number);	
+		} else {
+			System.out.println("출고 정보가 이미 존재합니다!");
+		}
 		return infoList;
 	}
 }
