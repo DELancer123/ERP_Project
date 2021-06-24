3e9c4881679b6f52835de7b45567e6555fa3591c
DESKTOP-I4AP5K0\82103 <tnsdnd5554@naver.com>
2021-06-24 16:47:36 +0900

    pom.xml json 추가 / 서블릿컨텍스트 빈추가 / 등등 원래있던 파일에 제 코드 복붙해서 옮겨넣었습니다.

---
+++ screen/webERP/src/main/java/com/myspring/StockManage/controller/StockManageControllerImpl.java
@@ -0,0 +1,136 @@
+package com.myspring.StockManage.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.myspring.StockManage.service.StockManageService;
+import com.myspring.StockManage.vo.StockManageVO;
+import com.myspring.productionBaseInfo.BOM.vo.bomVO;
+
+@Controller("StockManageController")
+public class StockManageControllerImpl implements StockManageController {
+	@Autowired
+	StockManageService stockManageservice;
+	@Autowired
+	StockManageVO stockVO;
+	@Autowired
+	SqlSession sqlSession;
+	@RequestMapping(value = "/member/curinvenstat.do", method = RequestMethod.GET)
+	public ModelAndView stockstatus(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = null;
+		String viewName = getViewName(request);
+		mav = new ModelAndView(viewName);
+		String type = request.getParameter("type_name");
+		String submit = request.getParameter("submit");
+		
+		StockManageVO searchVO = new StockManageVO();
+		
+			
+		
+		if (StringUtils.hasText(type)) {
+
+			List<StockManageVO> stockList = null;
+			stockList = stockManageservice.SearchStock(type);
+
+			mav.addObject("stockList", stockList);
+		}else {
+			List<StockManageVO> stockList = null;
+			stockList = stockManageservice.SearchStockAll();
+			mav.addObject("stockList", stockList);
+		}
+		
+		return mav;
+		}
+	@RequestMapping(value = "/member/curfinishedinvenstat.do", method = RequestMethod.GET)
+	public ModelAndView finishedstockstatus(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = null;
+		String viewName = getViewName(request);
+		mav = new ModelAndView(viewName);
+		String name = request.getParameter("type_name");
+		String submit = request.getParameter("submit");
+		
+		StockManageVO searchVO = new StockManageVO();
+		
+		
+			if (StringUtils.hasText(name)) {
+
+				List<StockManageVO> stockList = null;
+				stockList = stockManageservice.SearchfinishedStock(name);
+
+				mav.addObject("stockList", stockList);
+			}else {
+				List<StockManageVO> stockList = null;
+				stockList = stockManageservice.SearchfinishedStockAll();
+				mav.addObject("stockList", stockList);
+			}
+		
+		
+		return mav;
+		}
+	
+	@RequestMapping(value = "/member/searchtype.do", method = RequestMethod.GET)
+	public ModelAndView searchtype(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = getViewName(request);
+		List nameView = stockManageservice.nameView();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("nameView", nameView);
+
+		return mav;
+	}
+	@ResponseBody
+	@RequestMapping(value = "/member/searchPopName.do", method = RequestMethod.GET)
+	public ModelAndView searchPopName(@RequestParam("itemName") String itemName) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		List<StockManageVO> popName = null;
+		popName = stockManageservice.searchPopName(itemName);
+		mav.addObject("popName", popName);
+		mav.setViewName("jsonView");
+
+		return mav;
+	} 
+
+	private String getViewName(HttpServletRequest request) {
+		String contextPath = request.getContextPath();
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+		if (uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+		int begin = 0;
+		if (!((contextPath == null) || ("".equals(contextPath)))) {
+			begin = contextPath.length();
+		}
+
+		int end;
+		if (uri.indexOf(";") != -1) {
+			end = uri.indexOf(";");
+		} else if (uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		} else {
+			end = uri.length();
+		}
+
+		String viewName = uri.substring(begin, end);
+		if (viewName.indexOf(".") != -1) {
+			viewName = viewName.substring(0, viewName.lastIndexOf("."));
+		}
+		if (viewName.lastIndexOf("/") != -1) {
+			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
+		}
+		return viewName;
+	}
+
+}
---
+++ screen/webERP/src/main/java/com/myspring/StockManage/service/StockManageServiceImpl.java
@@ -0,0 +1,65 @@
+package com.myspring.StockManage.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.myspring.StockManage.dao.StockManageDAO;
+import com.myspring.StockManage.vo.StockManageVO;
+
+@Service("StockManageService")
+public class StockManageServiceImpl implements StockManageService{
+	
+	@Autowired
+	StockManageDAO stockManageDAO;
+
+	@Override
+	public List<StockManageVO> SearchStockAll() {
+		return stockManageDAO.SearchStockAll();
+	}
+
+	@Override
+	public List<StockManageVO> SearchfinishedStockAll() {
+		return stockManageDAO.SearchfinishedStockAll();
+	}
+
+	@Override
+	public List<StockManageVO> SearchStock(String type) { 
+		List popList = null;
+		popList = stockManageDAO.SearchStock(type);
+		
+		return popList;
+	}
+
+	@Override
+	public List<StockManageVO> SearchfinishedStock(String name) {
+		List popList = null;
+		popList = stockManageDAO.SearchfinishedStock(name);
+		
+		return popList;
+	}
+
+	@Override
+	public List nameView() {
+		List popList = null;
+		popList = stockManageDAO.nameView();
+		
+		return popList;
+	}
+
+	@Override
+	public List<StockManageVO> searchPopName(String itemName) {
+		return stockManageDAO.viewserachPopName(itemName);
+	}
+
+
+
+
+
+	
+
+	
+}
